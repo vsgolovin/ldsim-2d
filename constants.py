4b326f3e2a530594bb9969af8c695032aca6e23e
Vyacheslav Golovin <v.s.golovin@gmail.com>
2020-09-26 21:51:54 +0300

    Initial commit.

---
+++ constants.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 19:26:14 2020
+
+@author: vsgolovin
+"""
+
+T = 300                   # temperature (K)
+kb = 8.617333262145e-5    # Boltzmann constant (eV K-1)
+Vt = kb*T/1               # thermal potential (V or eV)
+eps_0 = 8.8541878128e-14  # vacuum permittivity (F cm-1)
+q = 1.60217662e-19        # elementary charge (C)
---
+++ statistics.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+"""
+A collection of statistical distribution functions for carrier density
+calculation.
+"""
+
+import numpy as np
+from fdint import fdk
+
+def fermi(nu):
+    """
+    Fermi-Dirac integral of order 1/2, uses `fdint` package.
+    """
+    F = fdk(k=0.5, phi=nu)
+    F *= 2/np.sqrt(np.pi)
+    return F
+
+def fermi_approx(nu):
+    """
+    Fermi-Dirac integral of order 1/2, uses approximate formula from
+    "Physics of Photonic Devices" by S.L.Chuang.
+    """
+    denom = nu + 2.13 + np.power(np.power(np.abs(nu-2.13), 12./5) + 9.6,
+                                 5./12)
+    C = 3*np.sqrt(np.pi/2) / np.power(denom, 1.5)
+    F = 1 / (np.exp(-nu) + C)
+    return F
+
+def blakemore(nu):
+    """
+    Use Blakemore approximation of Fermi-Dirac integral of order 1/2.
+    Valid for `nu`<=1.3.
+    """
+    F = 1 / (np.exp(-nu)+0.27)
+    return F
+
+def boltzmann(nu):
+    """
+    Use Boltzmann approximation of Fermi-Dirac integral of order 1/2.
+    Valid for `nu`<=-2.
+    """
+    F = np.exp(nu)
+    return F
+
