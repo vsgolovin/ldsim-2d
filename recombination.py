4c2832a840a026845e0249d2736f833570b10142
Vyacheslav Golovin <v.s.golovin@gmail.com>
2021-04-19 21:00:01 +0300

    Update recombination.py
    Replaced potentials with carrier densities in arguments of functions describing SRH recombination.

--- recombination.py
+++ recombination.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 """
 A collection of functions for calculating recombination rates and corresponding
 derivatives.
@@ -8,62 +7,22 @@ derivatives.
 import numpy as np
 import carrier_concentrations as cc
 
-def srh_R(psi, phi_n, phi_p, Nc, Nv, Ec, Ev, Vt, ni, n0, p0, tau_n, tau_p):
+def srh_R(n, p, n0, p0, tau_n, tau_p):
     """
     Shockley-Read-Hall recombination rate.
     """
-    n = cc.n(psi, phi_n, Nc, Ec, Vt)
-    p = cc.p(psi, phi_p, Nv, Ev, Vt)
-    R = (n*p - n0*p0) / ((n+ni)*tau_p + (p+ni)*tau_n)
+    R = (n*p - n0*p0) / ((n+n0)*tau_p + (p+p0)*tau_n)
     return R
 
-def srh_derivative(n, ndot, p, pdot, ni, n0, p0, tau_n, tau_p):
+def srh_derivative(n, ndot, p, pdot, n0, p0, tau_n, tau_p):
     """
     Auxillary function for repetative calculations.
     """
     u = n*p - n0*p0
-    v = (n+ni)*tau_p + (p+ni)*tau_n
+    v = (n+n0)*tau_p + (p+n0)*tau_n
     udot = ndot*p + n*pdot
     vdot = tau_p*ndot + tau_n*pdot
     Rdot = (udot*v - u*vdot) / v**2
-    return Rdot
-
-def srh_dR_dpsi(psi, phi_n, phi_p, Nc, Nv, Ec, Ev, Vt, ni, n0, p0,
-                tau_n, tau_p):
-    """
-    Shockley-Read-Hall recombination rate derivative with respect to potential.
-    """
-    n = cc.n(psi, phi_n, Nc, Ec, Vt)
-    ndot = cc.dn_dpsi(psi, phi_n, Nc, Ec, Vt)
-    p = cc.p(psi, phi_p, Nv, Ev, Vt)
-    pdot = cc.dp_dpsi(psi, phi_p, Nv, Ev, Vt)
-    Rdot = srh_derivative(n, ndot, p, pdot, ni, n0, p0, tau_n, tau_p)
-    return Rdot
-
-def srh_dR_dphin(psi, phi_n, phi_p, Nc, Nv, Ec, Ev, Vt, ni, n0, p0,
-                 tau_n, tau_p):
-    """
-    Shockley-Read-Hall recombination rate derivative with respect to electron
-    quasi Fermi potential.
-    """
-    n = cc.n(psi, phi_n, Nc, Ec, Vt)
-    ndot = cc.dn_dphin(psi, phi_n, Nc, Ec, Vt)
-    p = cc.p(psi, phi_p, Nv, Ev, Vt)
-    pdot = 0
-    Rdot = srh_derivative(n, ndot, p, pdot, ni, n0, p0, tau_n, tau_p)
-    return Rdot
-
-def srh_dR_dphip(psi, phi_n, phi_p, Nc, Nv, Ec, Ev, Vt, ni, n0, p0,
-                 tau_n, tau_p):
-    """
-    Shockley-Read-Hall recombination rate with respect to hole quasi Fermi
-    potential.
-    """
-    n = cc.n(psi, phi_n, Nc, Ec, Vt)
-    ndot = 0
-    p = cc.p(psi, phi_p, Nv, Ev, Vt)
-    pdot = cc.dp_dphip(psi, phi_p, Nv, Ev, Vt)
-    Rdot = srh_derivative(n, ndot, p, pdot, ni, n0, p0, tau_n, tau_p)
     return Rdot
 
 def rad_R(psi, phi_n, phi_p, Nc, Nv, Ec, Ev, Vt, n0, p0, B):
@@ -219,9 +178,10 @@ def stim_dR_dphip(psi, phi_n, phi_p, S, Nc, Nv, Ec, Ev, Vt, vg, g0, N_tr,
     return Rdot
 
 # testing
-test_err = 1e-3
+test_err = 1e-2
 
-def test_srh_derivatives():
+def test_srh_derivative():
+    # parameters
     psi = 24
     phi_n = 20
     phi_p = 19
@@ -230,50 +190,33 @@ def test_srh_derivatives():
     Ec = 55
     Ev = -1
     Vt = 1.4
-    ni = 2e7
     n0 = 2e7
     p0 = 1e7
     tau_n = 2
     tau_p = 3
 
-    # psi
-    psi_1 = psi*0.999
-    psi_2 = psi*1.001
-    R1 = srh_R(psi_1, phi_n, phi_p, Nc, Nv, Ec, Ev, Vt, ni, n0, p0,
-               tau_n, tau_p)
-    R2 = srh_R(psi_2, phi_n, phi_p, Nc, Nv, Ec, Ev, Vt, ni, n0, p0,
-               tau_n, tau_p)
+    # calculating SRH recombination rate derivative
+    # 1. by using `srh_derivative`
+    n = cc.n(psi, phi_n, Nc, Ec, Vt)
+    ndot = cc.dn_dpsi(psi, phi_n, Nc, Ec, Vt)
+    p = cc.p(psi, phi_p, Nv, Ev, Vt)
+    pdot = cc.dp_dpsi(psi, phi_p, Nv, Ev, Vt)
+    Rdot = srh_derivative(n, ndot, p, pdot, n0, p0, tau_n, tau_p)
+
+    # 2. by using finite differences
+    psi_1 = psi*(1-1e-3)
+    psi_2 = psi*(1+1e-3)
+    n1 = cc.n(psi_1, phi_n, Nc, Ec, Vt)
+    p1 = cc.p(psi_1, phi_p, Nv, Ev, Vt)
+    R1 = srh_R(n1, p1, n0, p0, tau_n, tau_p)
+    n2 = cc.n(psi_2, phi_n, Nc, Ec, Vt)
+    p2 = cc.p(psi_2, phi_p, Nv, Ev, Vt)
+    R2 = srh_R(n2, p2, n0, p0, tau_n, tau_p)
     Rdot_fd = (R2-R1)/(psi_2-psi_1)
-    Rdot = srh_dR_dpsi(psi, phi_n, phi_p, Nc, Nv, Ec, Ev, Vt, ni, n0, p0,
-                       tau_n, tau_p)
-    err_psi = abs(1-Rdot/Rdot_fd)
-
-    # phi_n
-    phi_n1 = phi_n*0.999
-    phi_n2 = phi_n*1.001
-    R1 = srh_R(psi, phi_n1, phi_p, Nc, Nv, Ec, Ev, Vt, ni, n0, p0,
-               tau_n, tau_p)
-    R2 = srh_R(psi, phi_n2, phi_p, Nc, Nv, Ec, Ev, Vt, ni, n0, p0,
-               tau_n, tau_p)
-    Rdot_fd = (R2-R1)/(phi_n2-phi_n1)
-    Rdot = srh_dR_dphin(psi, phi_n, phi_p, Nc, Nv, Ec, Ev, Vt, ni, n0, p0,
-                        tau_n, tau_p)
-    err_phin = abs(1-Rdot/Rdot_fd)
-
-    # phi_p
-    phi_p1 = phi_p*0.999
-    phi_p2 = phi_p*1.001
-    R1 = srh_R(psi, phi_n, phi_p1, Nc, Nv, Ec, Ev, Vt, ni, n0, p0,
-               tau_n, tau_p)
-    R2 = srh_R(psi, phi_n, phi_p2, Nc, Nv, Ec, Ev, Vt, ni, n0, p0,
-               tau_n, tau_p)
-    Rdot_fd = (R2-R1)/(phi_p2-phi_p1)
-    Rdot = srh_dR_dphip(psi, phi_n, phi_p, Nc, Nv, Ec, Ev, Vt, ni, n0, p0,
-                        tau_n, tau_p)
-    err_phip = abs(1-Rdot/Rdot_fd)
 
     # checking results
-    assert err_psi < test_err and err_phin < test_err and err_phip < test_err
+    err = abs(1-Rdot/Rdot_fd)
+    assert err < test_err
 
 def test_rad_derivatives():
     psi = 24
